ed6126f9abaa686a5aef8d4d6d1e09233523c0ab
Guilherme Torres <53585353+guibbers@users.noreply.github.com>
2021-12-14 22:07:49 -0300

    finish lesson 4

--- index.js
+++ index.js
@@ -24,9 +24,18 @@ function operation() {
     .then((answer) => {
       const action = answer['action']
 
-      console.log(action)
+      if (action === 'Criar Conta') {
+        createAccount()
+      }
     })
     .catch((err) => {
       console.error(err)
     })
+}
+
+// Create Account
+
+function createAccount() {
+  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+  console.log(chalk.green('Defina as opções da sua conta a seguir: '))
 }
