a710d0712bf78a85e2715a4399b71cbbbc497142
Guilherme Torres <53585353+guibbers@users.noreply.github.com>
2021-12-14 22:21:34 -0300

    finish exit action

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ function operation() {
 
       if (action === 'Criar Conta') {
         createAccount()
+      } else if (action === 'Consultar Saldo') {
+        return
+      } else if (action === 'Depositar') {
+        return
+      } else if (action === 'Sacar') {
+        return
+      } else if (action === 'Sair') {
+        console.log(chalk.bgBlue.black('Obrigar por usar o Accounts!'))
+        process.exit()
       }
     })
     .catch((err) => {
@@ -36,6 +45,45 @@ function operation() {
 // Create Account
 
 function createAccount() {
-  console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
-  console.log(chalk.green('Defina as opções da sua conta a seguir: '))
+  buildAccount()
+}
+
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta: ',
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer['accountName']
+      console.info(accountName)
+
+      if (!fs.existsSync('accounts')) {
+        fs.mkdirSync('accounts')
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.error(
+          chalk.bgRed.black('Esta conta já existe, escolha outro nome!')
+        )
+        buildAccount()
+        return
+      }
+
+      fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        '{"balance": 0}',
+        (err) => {
+          console.error(err)
+        }
+      )
+
+      console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+      operation()
+    })
+    .catch((err) => {
+      console.error(err)
+    })
 }
